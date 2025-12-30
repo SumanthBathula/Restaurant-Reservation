05f5ed18bc53081d9b928516e00a8af655486efc
SumanthBathula <sumanthbittu16@gmail.com>
2025-12-30 15:01:37 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 
-const API_URL =
-  process.env.REACT_APP_API_URL ||
-  'https://restaurant-reservation-backend-usr5.vercel.app';
+const API_URL = "https://restaurant-reservation-backend-usr5.vercel.app";
 
 
 function App() {
